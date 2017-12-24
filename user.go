71e9de08013bba9ad2aef5156dd415373ec2ca11
ninedraft <>
2017-12-24 16:57:51 +0300

    adds uuid

--- user.go
+++ user.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"github.com/satori/go.uuid"
 	"time"
 )
 
-type User struct {
-	UUID             string
+type user struct {
+	UUID             uuid.UUID
 	Login            string
 	RegistrationDate time.Time
 }
