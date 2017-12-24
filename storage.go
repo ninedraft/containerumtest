b386f2075d39480dcd19e33977de2de89b6e4754
ninedraft <>
2017-12-24 17:30:44 +0300

    storage CreateUser method

--- storage.go
+++ storage.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"github.com/asdine/storm"
+	"github.com/satori/go.uuid"
 )
 
 type storage struct {
 	db *storm.DB
+}
+
+func (sto *storage) CreateUser(config *UserConfig) (*uuid.UUID, error) {
+	user := config.User()
+	err := sto.db.Save(user)
+	if err != nil {
+		return nil, err
+	}
+	return &user.UUID, nil
 }
--- user.go
+++ user.go
@@ -5,23 +5,28 @@ import (
 	"time"
 )
 
-type user struct {
+// User struct.
+// To create User instance, use UserConfig
+type User struct {
 	UUID             uuid.UUID `storm:"unique"`
 	Login            string    `storm:"index"`
 	RegistrationDate time.Time `storm:"index"`
 }
 
-type userConfig struct {
+// UserConfig creates new User
+type UserConfig struct {
 	Login            string     `json:"login"`
 	RegistrationDate *time.Time `json:"registration_date"`
 }
 
-func (config *userConfig) User() *user {
+// User method emits new User from UserConfig with same Login field.
+// If RegistrationDate is nil, then time.Now() is used.
+func (config *UserConfig) User() *User {
 	if config.RegistrationDate == nil {
 		now := time.Now()
 		config.RegistrationDate = &now
 	}
-	return &user{
+	return &User{
 		UUID:             uuid.NewV4(),
 		Login:            config.Login,
 		RegistrationDate: *config.RegistrationDate,
