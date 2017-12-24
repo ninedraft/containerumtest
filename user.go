5de184b202ee01f5817699af595a9c3672a77f9b
ninedraft <>
2017-12-24 17:21:14 +0300

    user config

--- user.go
+++ user.go
@@ -10,3 +10,20 @@ type user struct {
 	Login            string    `storm:"index"`
 	RegistrationDate time.Time `storm:"index"`
 }
+
+type userConfig struct {
+	Login            string     `json:"login"`
+	RegistrationDate *time.Time `json:"registration_date"`
+}
+
+func (config *userConfig) User() *user {
+	if config.RegistrationDate == nil {
+		now := time.Now()
+		config.RegistrationDate = &now
+	}
+	return &user{
+		UUID:             uuid.NewV4(),
+		Login:            config.Login,
+		RegistrationDate: *config.RegistrationDate,
+	}
+}
