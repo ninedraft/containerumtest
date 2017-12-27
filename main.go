9c050740c28ed808e8f9a6958ca4e94c12171710
ninedraft <>
2017-12-27 23:06:05 +0300

    main func

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"log"
+)
 
+func main() {
+	filename := flag.String("db", "user.db", "DB file")
+	storage, err := NewStorage(*filename)
+	if err != nil {
+		log.Fatalf("error while creating storage: %v\n", err)
+	}
+	service, err := NewService(storage)
+	if err != nil {
+		log.Fatalf("error while creating service: %v\n", err)
+	}
+	err = service.Start(":8222")
+	if err != nil {
+		log.Fatalf("error while startinggo service: %v\n", err)
+	}
 }
